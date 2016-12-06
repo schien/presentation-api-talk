c59149dbec55bdb7c4327ed0ea7d528b7222be48
Shih-Chiang Chien <schien@mozilla.com>
2016-12-05 17:39:24 -1000

    initial version

---
+++ plugin/remote-control/receiver.js
@@ -0,0 +1,71 @@
+var RemoteControl = (function() {
+  if (!navigator.presentation || ! navigator.presentation.receiver) {
+    console.warn('not in receiver window');
+    return {};
+  }
+
+  var conn;
+
+  function post() {
+    if (!conn) {
+      console.error('presentation connection is not available');
+      return;
+    }
+    var slideElement = Reveal.getCurrentSlide();
+    var notesElement = slideElement.querySelector( 'aside.notes' );
+
+    var messageData = {
+      notes: '',
+      markdown: false,
+      whitespace: 'normal',
+      state: Reveal.getState()
+    };
+
+    // Look for notes defined in a slide attribute
+    if( slideElement.hasAttribute( 'data-notes' ) ) {
+      messageData.notes = slideElement.getAttribute( 'data-notes' );
+      messageData.whitespace = 'pre-wrap';
+    }
+
+    // Look for notes defined in an aside element
+    if( notesElement ) {
+      messageData.notes = notesElement.innerHTML;
+      messageData.markdown = typeof notesElement.getAttribute( 'data-markdown' ) === 'string';
+    }
+
+    var msg = {
+      type: 'status',
+      data: messageData,
+    }
+
+    conn.send(JSON.stringify(msg));
+  }
+
+  function onCommand(cmd) {
+    switch (cmd.type) {
+      case 'next':
+        Reveal.next();
+        break;
+      case 'prev':
+        Reveal.prev();
+        break;
+      default:
+        console.warn('unknown command: ' + JSON.stringify(cmd));
+        return;
+    }
+    post();
+  }
+
+  navigator.presentation.reciever.connectionList
+  .then(function(connList) {
+    conn = connList.connections[0];
+    conn.addEventListener('message', function(event) {
+      console.log('on message: ' + event.data);
+      var cmd = JSON.parse(event.data);
+      onCommand(cmd);
+    });
+    post();
+  }).catch(function(e) {
+    console.error('reciever init error: ' + e);
+  });
+})();
